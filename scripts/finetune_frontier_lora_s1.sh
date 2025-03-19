334dffaf9b279dedec8987dc2e8ceda37cdd5521
Gary3410 <92074345+Gary3410@users.noreply.github.com>
2025-03-19 11:44:01 +0800

    Add files via upload

---
+++ scripts/finetune_frontier_lora_s1.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# IMPORTANT: this is the training script for the original LLaVA, NOT FOR LLaVA V1.5!
+
+# Uncomment and set the following variables correspondingly to run this script:
+################## ALL ##################
+HF_DATASETS_OFFLINE=1
+TRANSFORMERS_OFFLINE=1
+gpu_vis=5,6
+# --vision_tower will be the local file path
+################## VICUNA ##################
+PROMPT_VERSION=v1
+MODEL_VERSION="vicuna-v1-3-7b"
+# MODEL_VERSION="vicuna-13b-v1.5"
+################## VICUNA ##################
+
+################## LLaMA-2 ##################
+# PROMPT_VERSION="llava_llama_2"
+# MODEL_VERSION="llama-2-7b-chat"
+################## LLaMA-2 ##################
+
+deepspeed --include localhost:$gpu_vis llava/train/train_frontier.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --version $PROMPT_VERSION \
+    --data_path ./data/llava_vln_parsed_response_100_v8_easy_train_s1_v4.json \
+    --image_folder ./vision_dataset/llava_dataset_v8_easy_train/frontiers_feature \
+    --vision_tower ./checkpoints/clip-vit-large-patch14 \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir ./checkpoints_output/llava-$MODEL_VERSION-finetune-planner-lora-s1-v4 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
